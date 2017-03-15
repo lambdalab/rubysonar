6cec2ca17a40910b530f58313e2025b3bf9fb693
DexinLi <ldx94@hotmail.com>
2017-03-15 11:41:59 +0800

    requested changes

--- src/test/java/org/yinwang/rubysonar/TestRefs.java
+++ src/test/java/org/yinwang/rubysonar/TestRefs.java
@@ -1,23 +1,17 @@
 package org.yinwang.rubysonar;
-
 import org.junit.Test;
-
 import static org.junit.Assert.fail;
-
 import java.util.List;
 
 public class TestRefs
 {
     @Test
-    public void testRefs()
-    {
+    public void testRefs(){
         List<String> failed = org.yinwang.rubysonar.Test.testAll("tests", false);
-        if (failed != null)
-        {
+        if (failed != null){
             String msg = "Some tests failed. ";
             msg += "\n----------------------------- FAILED TESTS ---------------------------";
-            for (String fail : failed)
-            {
+            for (String fail : failed){
                 msg += "\n - " + fail;
             }
             msg += "\n----------------------------------------------------------------------";
