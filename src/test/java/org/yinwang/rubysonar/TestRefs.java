ec6907940c7c3293e29f7a59c232ab348ba96058
DexinLi <ldx94@hotmail.com>
2017-03-15 11:03:55 +0800

    Test
    Build a test framework and write 4 test files that each one raises a
    bug.

---
+++ src/test/java/org/yinwang/rubysonar/TestRefs.java
@@ -0,0 +1,27 @@
+package org.yinwang.rubysonar;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+import java.util.List;
+
+public class TestRefs
+{
+    @Test
+    public void testRefs()
+    {
+        List<String> failed = org.yinwang.rubysonar.Test.testAll("tests", false);
+        if (failed != null)
+        {
+            String msg = "Some tests failed. ";
+            msg += "\n----------------------------- FAILED TESTS ---------------------------";
+            for (String fail : failed)
+            {
+                msg += "\n - " + fail;
+            }
+            msg += "\n----------------------------------------------------------------------";
+            fail(msg);
+        }
+    }
+}
---
+++ tests/branch.test/branch.rb
@@ -0,0 +1,11 @@
+
+#do not merge at the end
+#fixed in pysonar
+x=1
+y="1"
+if x>1
+    y=1
+else
+    y=2
+end
+z=y
