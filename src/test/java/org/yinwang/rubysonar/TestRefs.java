e962fd876dfa182ca6f1a167b5c439b4d37f373d
DexinLi <ldx94@hotmail.com>
2017-03-15 11:48:01 +0800

    blah

--- src/test/java/org/yinwang/rubysonar/TestRefs.java
+++ src/test/java/org/yinwang/rubysonar/TestRefs.java
@@ -3,8 +3,7 @@ import org.junit.Test;
 import static org.junit.Assert.fail;
 import java.util.List;
 
-public class TestRefs
-{
+public class TestRefs{
     @Test
     public void testRefs(){
         List<String> failed = org.yinwang.rubysonar.Test.testAll("tests", false);
